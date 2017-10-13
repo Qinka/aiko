3c00b73474f83cb48ab67fad798de9fa64f2fb2a
Qinka <qinka@live.com>
2017-10-13 19:07:45 +0800

    update

--- source/startup/boot.s
+++ source/startup/boot.s
@@ -50,37 +50,37 @@ defined in linker script */
 .weak Reset_System
 .type Reset_System, %function
 Reset_System:
-	/* Copy the data segment initializers from flash to SRAM */
-	movs r1, #0
-  b LoopCopyDataInit
 
-LoopCopyDataInit:	
-	ldr r0, =_sdata	
-	ldr r3, =_edata
-	adds r2, r0, r1
-	cmp r2, r3
-	bcc CopyDataInit
-	ldr r2, =_sbss
-	b LoopFillZerobss
-  
+/* Copy the data segment initializers from flash to SRAM */  
+  movs  r1, #0
+  b  LoopCopyDataInit
+
 CopyDataInit:
-	ldr r3, =_sidata
-	ldr r3, [r3, r1]
-	str r3, [r0, r1]
-	adds r1, r1, #4
-		
-LoopFillZerobss:
-	ldr r3, = _ebss
-	cmp r2, r3
-	bcc FillZerobss
-/* Call the aiko's entry point: aiko_initializtion */
-	bl aiko_initialization
-	bx lr
-
+  ldr  r3, =_sidata
+  ldr  r3, [r3, r1]
+  str  r3, [r0, r1]
+  adds  r1, r1, #4
+    
+LoopCopyDataInit:
+  ldr  r0, =_sdata
+  ldr  r3, =_edata
+  adds  r2, r0, r1
+  cmp  r2, r3
+  bcc  CopyDataInit
+  ldr  r2, =_sbss
+  b  LoopFillZerobss
 /* Zero fill the bss segment. */  
 FillZerobss:
-	movs  r3, #0
-	str  r3, [r2], #4 
+  movs  r3, #0
+  str  r3, [r2], #4
+    
+LoopFillZerobss:
+  ldr  r3, = _ebss
+  cmp  r2, r3
+  bcc  FillZerobss
+/* Call the application's entry point.*/
+  bl  aiko_initialization
+  bx  lr    
 .size Reset_System, .-Reset_System
 
 /**
