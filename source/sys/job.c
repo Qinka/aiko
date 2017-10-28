3e94a486f84cd82d3ae16e074209990b675935dc
Johann Lee <qinka@live.com>
2017-10-28 16:40:16 +0800

    update

--- source/sys/job.c
+++ source/sys/job.c
@@ -17,19 +17,26 @@ static struct job_t *job_queue_head_p = 0;
 static struct job_t *job_queue_tail_p = 0;
 
 /// the wait queue
-static struct wait_t *wait_queue = 0;
+static struct wait_t wait_queue = {
+    .wait_job = 0,
+    .wait_time = 0,
+    .next_wait_p = 0};
 
 /**
  * add a job to queue
  * @param j the job to be added
  */
-int __job_add(struct job_t *j) {
-  if (job_queue_tail_p) {
+int __job_add(struct job_t *j)
+{
+  if (job_queue_tail_p)
+  {
     // job queue not empty
     job_queue_tail_p->next_job_p = j;
     job_queue_tail_p = j;
     job_queue_tail_p->next_job_p = 0;
-  } else {
+  }
+  else
+  {
     // job queue is empty
     job_queue_head_p = j;
     job_queue_tail_p = j;
@@ -42,12 +49,13 @@ int __job_add(struct job_t *j) {
  * add a job to wait queue
  * @param j the job to be added
  */
-int __wait_add(struct job_t *j, int8_t t) {
+int __wait_add(struct job_t *j, int8_t t)
+{
   struct wait_t *w = (struct wait_t *)malloc(sizeof(struct wait_t));
   w->wait_job = j;
   w->wait_time = t;
-  w->next_wait_p = wait_queue;
-  wait_queue = w;
+  w->next_wait_p = wait_queue->next_wait_p;
+  wait_queue->next_wait_p = w;
   return 0;
 }
 
@@ -55,10 +63,14 @@ int __wait_add(struct job_t *j, int8_t t) {
  * add a job to queue
  * @param j the job to be added
  */
-int job_add(struct job_t *j, int8_t w) {
-  if (w < 0) {
+int job_add(struct job_t *j, int8_t w)
+{
+  if (w < 0)
+  {
     __job_add(j);
-  } else {
+  }
+  else
+  {
     __wait_add(j, w);
   }
   return 0;
@@ -69,48 +81,56 @@ int job_add(struct job_t *j, int8_t w) {
  * @return the point for job_t
  */
 
-struct job_t *job_get() {
+struct job_t *job_get()
+{
   struct job_t *rt;
-  if (job_queue_head_p) {
+  if (job_queue_head_p)
+  {
     // job queue not empty
     rt = job_queue_head_p;
     if (job_queue_head_p == job_queue_tail_p)
       job_queue_tail_p = 0;
     job_queue_head_p = job_queue_head_p->next_job_p;
-  } else
+  }
+  else
     rt = job_queue_head_p;
   return rt;
 }
 
-int job_scheduler_launch() {
-  if (!job_s_lock) {
+int job_scheduler_launch()
+{
+  if (!job_s_lock)
+  {
     job_s_lock = 1;
     // loop
-    while (1) {
+    while (1)
+    {
       struct job_t *cur = job_get();
       if (cur)
         cur->job_method(cur);
     }
     job_s_lock = 0;
     return 0;
-  } else
+  }
+  else
     return 1;
 }
-void wait_handler(void) {
+void wait_handler(void)
+{
   struct wait_t *curr = wait_queue;
-  struct wait_t *last = wait_queue;
-  while (curr) {
-    if (curr->wait_time > 0) {
-      curr->wait_time -= 1;
-      last = curr;
-    } else {
-      if (last != curr)
-        last->next_wait_p = curr->next_wait_p;
-      else
-        wait_queue = curr->next_wait_p;
-      __job_add(curr->wait_job);
-      free(curr);
+  while (curr->next_wait_p)
+  {
+    struct wait_t *this = curr->next_wait_p;
+    if (this->wait_time > 0)
+    {
+      this->wait_time -= 1;
+      curr = curr->next_wait_p;
     }
-    curr = curr->next_wait_p;
+    else
+    {
+      __job_add(this->wait_job);
+      curr->next_wait_p = this->next_wait_p;
+      free(this);
+    }
   }
 }
