0a0ce174c24265002084b08abe5f41704f3f8175
Johann Lee <qinka@live.com>
2017-10-28 16:55:00 +0800

    update

--- config/compiler.cmake
+++ config/compiler.cmake
@@ -25,7 +25,7 @@ set (AIKO_CONFIG_DEV "STM32F10X_HD" CACHE string "the kind of device")
 ## set up compiler
 set (CMAKE_C_COMPILER   "${AIKO_ARM_PREFIX}gcc")
 set (CMAKE_CXX_COMPILER "${AIKO_ARM_PREFIX}g++")
-set (CMAKE_C_FLAGS      "$ENV{CCFLAGS} ${INNER_AIKO_CONFIG_DEBUG}  -mcpu=cortex-m3 -mthumb -nostdlib")
+set (CMAKE_C_FLAGS      "$ENV{CCFLAGS} ${INNER_AIKO_CONFIG_BUG}  -mcpu=cortex-m3 -mthumb -nostdlib")
 set (CMAKE_ASM_COMPILER "${AIKO_ARM_PREFIX}gcc")
 set (CMAKE_AR "${AIKO_ARM_PREFIX}ar")
 set (CMAKE_RANLIB "${AIKO_ARM_PREFIX}ranlib")
